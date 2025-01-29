80530ef8593862e777ca5e76454609ebed0e8b2a
Jishiro <kronosiraxx@gmail.com>
2025-01-29 14:16:02 -0600

    Fondo estrella

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-let videos = ['video1.mp4', 'video2.mp4', 'video3.mp4'];
-let currentVideoIndex = 0;
-const videoPlayer = document.getElementById('videoPlayer');
+function createStars(num) {
+    const starsContainer = document.querySelector('.stars');
+    for (let i = 0; i < num; i++) {
+        let star = document.createElement("div");
+        star.classList.add("star");
+        let x = Math.random() * window.innerWidth;
+        let y = Math.random() * window.innerHeight;
+        let delay = Math.random() * 5;
+        let size = Math.random() * 1 + 2;
 
-function prevVideo() {
-    currentVideoIndex = (currentVideoIndex === 0) ? videos.length - 1 : currentVideoIndex - 1;
-    videoPlayer.src = videos[currentVideoIndex];
-    videoPlayer.play();
+        star.style.left = `${x}px`;
+        star.style.top = `${y}px`;
+        star.style.width = `${size}px`;
+        star.style.height = `${size}px`;
+        star.style.animationDuration = `${4 + Math.random() * 3}s`;
+        star.style.animationDelay = `${delay}s`;
+
+        starsContainer.appendChild(star);
+    }
 }
 
-function nextVideo() {
-    currentVideoIndex = (currentVideoIndex === videos.length - 1) ? 0 : currentVideoIndex + 1;
-    videoPlayer.src = videos[currentVideoIndex];
-    videoPlayer.play();
-}
+createStars(120);
